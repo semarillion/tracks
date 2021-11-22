932e0a04a9337d47606ef994666a1813afdffcf9
semarillion <anonymous@mailbox.org>
2021-11-22 05:34:10 +0100

    Revert "plot appropriate grid as well as tracks"
    This reverts commit 2e466a2e2e3d36550188bd81125e4d284d02764c.

--- tracks_overlap_fast.py
+++ tracks_overlap_fast.py
@@ -302,8 +302,7 @@ lat_lon_pd['lon']=sum_lon
 
 # make the initial stacked array which helds all values for lat an long
 X = np.c_[sum_lat, sum_lon]
-X_all = np.c_[sum_lon,sum_lat]
-#np.savetxt('X.csv',X_all,delimiter=',')
+X_all = X.copy()
 LEN_ALL_POINTS = len(X)
 
 # make dictionary of ranges for each track, because they are her available in one list
